79c1aa39205ab0460df21b45790d4d39a81ee6c1
anasalashqar <anasalashqar16@gmail.com>
2025-02-02 21:34:18 +0300

    added task 21

---
+++ Task21(JavaScript-Exercises-Basic)/main.js
@@ -0,0 +1,112 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const one = document.getElementById("one");
+  const two = document.getElementById("two");
+  const r = JSON.parse(localStorage.getItem("append")) || 1;
+  if (1 == r) {
+    one.style.display = "flex";
+    two.style.display = "none";
+  } else {
+    one.style.display = "none";
+    two.style.display = "flex";
+  }
+});
+
+document.getElementById("form1").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const passwordConfirm = document.getElementById("confirmPassword").value;
+  const emailConfirm = document.getElementById("confirmEmail").value;
+
+  if (email !== emailConfirm) {
+    document.getElementById("answer").style.color = "red";
+    document.getElementById("answer").innerHTML =
+      "Youre email doesnt match the confirmed one try again";
+  }
+  if (password !== passwordConfirm) {
+    document.getElementById("answer").style.color = "red";
+    document.getElementById("answer").innerHTML =
+      "Youre password doesnt match the confirmed one try again";
+  }
+  if (
+    email &&
+    password &&
+    emailConfirm &&
+    passwordConfirm &&
+    password === passwordConfirm &&
+    email === emailConfirm
+  ) {
+    localStorage.setItem(
+      "answer",
+      JSON.stringify({ email: email, password: password })
+    );
+    appending(2);
+    localStorage.setItem("append", "2");
+    form.reset();
+  }
+});
+
+const form = document.getElementById("form1");
+
+form.addEventListener("input", () => {
+  document.getElementById("submit").disabled = true;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const passwordConfirm = document.getElementById("confirmPassword").value;
+  const emailConfirm = document.getElementById("confirmEmail").value;
+  if (email && password && emailConfirm && passwordConfirm) {
+    document.getElementById("submit").disabled = false;
+  }
+});
+
+document.getElementById("form2").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email2 = document.getElementById("email2").value || "";
+  const password2 = document.getElementById("password2").value || "";
+
+  let r = JSON.parse(localStorage.getItem("answer"));
+
+  if (r == null) {
+    document.getElementById("answer2").style.color = "red";
+    document.getElementById("answer2").innerHTML = "You dont have an account!";
+  }
+  if (email2 !== r.email) {
+    document.getElementById("answer2").style.color = "red";
+    document.getElementById("answer2").innerHTML =
+      "Youre email doesnt match the confirmed one try again";
+  }
+
+  if (password2 != r.password) {
+    document.getElementById("answer2").style.color = "red";
+    document.getElementById("answer2").innerHTML =
+      "Youre pass doesnt match the confirmed one try again";
+  }
+  if (email2 && password2 && password2 === r.password && email2 === r.email) {
+    document.getElementById("answer2").style.color = "green";
+    document.getElementById("answer2").innerHTML =
+      "Form Submitted Successfully";
+    alert("Congrats you are logged in successfully!");
+  }
+});
+
+document.getElementById("btn1").addEventListener("click", () => {
+  appending(2);
+  localStorage.setItem("append", "2");
+});
+document.getElementById("btn2").addEventListener("click", () => {
+  appending(1);
+  localStorage.setItem("append", "1");
+});
+
+const appending = (pageNumber) => {
+  const one = document.getElementById("one");
+  const two = document.getElementById("two");
+
+  if (pageNumber == 1) {
+    one.style.display = "flex";
+    two.style.display = "none";
+  } else if (pageNumber == 2) {
+    one.style.display = "none";
+    two.style.display = "flex";
+  }
+};
